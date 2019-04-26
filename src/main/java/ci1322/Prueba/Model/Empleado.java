b740a37ea1ebbdbfdd117b6e6a7f5243f1a9b204
esteban201483 <Esteban201483@gmail.com>
2019-04-25 23:32:54 -0600

    Fix: Renombra paquetes para diferenciarlos de los paquetes del generador de codigo dml

---
+++ src/main/java/ci1322/Prueba/Model/Empleado.java
@@ -0,0 +1,11 @@
+package ci1322.Prueba.Model;
+
+import ci1322.Tarea1.Annotations.Entity;
+import ci1322.Tarea1.Annotations.Table;
+
+@Entity
+@Table(name = "Empleado")
+public class Empleado {
+    
+}
+
---
+++ src/main/java/ci1322/Prueba/Model/Restaurante.java
@@ -0,0 +1,12 @@
+package ci1322.Prueba.Model;
+
+
+import ci1322.Tarea1.Annotations.Entity;
+import ci1322.Tarea1.Annotations.Table;
+
+@Entity
+@Table(name = "Restaurante")
+public class Restaurante {
+
+
+}
