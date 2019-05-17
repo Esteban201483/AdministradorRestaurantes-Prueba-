848fc54ed13123b0dbfc8c8e197f4d0ec0ede240
Esteban De Jesus Rojas Solis <b66293@ecci.ucr.ac.cr>
2019-05-17 14:31:45 -0600

    Fix: Instancia

--- src/main/java/Prueba2.java
+++ src/main/java/Prueba2.java
@@ -4,8 +4,8 @@ public class Prueba2 {
     public static void main(String args[])
     {
         System.out.println("Prueba");
-        Prueba.main(args);
-
+        Prueba p = new Prueba();
+        p.encontrarClases("ci1322.Prueba.Model");
 
     }
 }
